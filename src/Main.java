f7f5203f966f0c771eb017d1e2c2e06f6cfbd5ec
trzcioli <trzcioli@gmail.com>
2019-04-02 20:36:55 +0200

    add instances

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main {
 
     public static void main(String[] args) {
+        if(args.length != 1) {
+            System.err.println("Invalid command line, exactly one argument required");
+            System.exit(1);
+        }
+
+        try {
+            FileInputStream fstream = new FileInputStream(args[0]);
+            System.out.println(fstream);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         //load + matrix
 
         //gen_alg
