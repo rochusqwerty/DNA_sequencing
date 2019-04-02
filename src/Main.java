593524029149da05467715e23095e1ba68013e61
trzcioli <trzcioli@gmail.com>
2019-04-02 22:24:56 +0200

    add load matrix with weights - depends on shift

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,50 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
 public class Main {
 
     public static void main(String[] args) {
-        if(args.length != 1) {
+        if (args.length != 1) {
             System.err.println("Invalid command line, exactly one argument required");
             System.exit(1);
         }
 
-        try {
-            FileInputStream fstream = new FileInputStream(args[0]);
-            System.out.println(fstream);
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
+        List<String> list = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        //load + matrix
+
+
+        int[][] matrix = new int [list.size()][list.size()];
+
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                String l = list.get(i);
+                String l2 = list.get(j);
+                if (l.substring(0, 8).equals(l2.substring(1, 9))) {
+                    matrix[i][j] = 1;
+                } else if (l.substring(0, 7).equals(l2.substring(2, 9))) {
+                    matrix[i][j] = 2;
+                } else if (l.substring(0, 6).equals(l2.substring(3, 9))) {
+                    matrix[i][j] = 3;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
 
         //gen_alg
 
