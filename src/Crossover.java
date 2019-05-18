7029513986f366ae663a33ad05dea117f4b583ce
rochusqwerty <rochus96@gmail.com>
2019-05-18 17:11:03 +0200

    Clean Crossover

--- src/Crossover.java
+++ src/Crossover.java
@@ -19,35 +19,32 @@ public class Crossover {
         max = listIn.size();
         bound *= (float) max;
         for(int i = 0; i< bound; i++){
+            //wybór pierwszego
             randomNum = ThreadLocalRandom.current().nextInt(min, max);
-            chromo1.clear();
-            chromo2.clear();
             chromo1 = new ArrayList<>(listIn.get(randomNum).getList_gens());
+            //wybór środkowego elementu
             middle = chromo1.size() / 2;
+            sample = 5; //TODO def parametrów w main
             while(sample > 0){
                 sample--;
+                //wybór drugiego`
                 randomNum1 = ThreadLocalRandom.current().nextInt(min, max);
                 while(randomNum == randomNum1){
                     randomNum1 = ThreadLocalRandom.current().nextInt(min, max);
                 }
                 chromo2 = new ArrayList<>(listIn.get(randomNum1).getList_gens());
 
+                //dopasowanie miejsca cięcia drugiego
                 steps = (int) (chromo2.size()-1);
                 middle1 = chromo2.size() / 2;
                 if((chromo2.size() > 2) && (chromo1.size() > 2)) {
                     for (int q = 0; q < steps; q++) {
-                        System.out.println("Dane");
-                        System.out.println(chromo1.size());
-                        System.out.println(middle);
-                        System.out.println(chromo2.size());
-                        System.out.println(middle1);
                         index = middle1 + (q % 2 == 0 ? q / 2 : -(q / 2 + 1));
 
                         if (chromo1.get(middle).equals(chromo2.get(index))) {
                             sample = 0;
 
                             newChromo = new Chromosome();
-                            newList.clear();
                             newList = new ArrayList<>(chromo1);
                             newList = newList.subList(0, middle);
                             newList2 = new ArrayList<>(chromo2);
